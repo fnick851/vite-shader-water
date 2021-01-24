3dd233ba200e942e27a21dcd4ee6670fe617a11d
Noah <hanlun.song@gmail.com>
2021-01-24 15:49:50 -0500

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template></template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
