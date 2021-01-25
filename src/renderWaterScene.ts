3ac835c9a199348a56eaf22cb7e43e6af90275dd
Noah <hanlun.song@gmail.com>
2021-01-25 00:19:41 -0500

    refactor

--- src/renderWaterScene.ts
+++ src/renderWaterScene.ts
@@ -15,138 +15,151 @@ import {
 import waterVertexShader from "./shaders/water/vertex";
 import waterFragmentShader from "./shaders/water/fragment";
 
+/**
+ * Base
+ */
+
+// Canvas
+
+// Scene
+const scene = new Scene();
+
+/**
+ * Water
+ */
+// Geometry
+const waterGeometry = new PlaneBufferGeometry(2, 2, 512, 512);
+
+// Colors
+const debugObject = {
+  depthColor: "#186691",
+  surfaceColor: "#9bd8ff",
+};
+
+// Material
+const waterMaterial = new ShaderMaterial({
+  vertexShader: waterVertexShader,
+  fragmentShader: waterFragmentShader,
+  uniforms: {
+    uTime: { value: 0 },
+
+    uBigWavesElevation: { value: 0.2 },
+    uBigWavesFrequency: { value: new Vector2(4, 1.5) },
+    uBigWavesSpeed: { value: 0.75 },
+
+    uSmallWavesElevation: { value: 0.15 },
+    uSmallWavesFrequency: { value: 3 },
+    uSmallWavesSpeed: { value: 0.2 },
+    uSmallIterations: { value: 4 },
+
+    uDepthColor: { value: new Color(debugObject.depthColor) },
+    uSurfaceColor: { value: new Color(debugObject.surfaceColor) },
+    uColorOffset: { value: 0.08 },
+    uColorMultiplier: { value: 5 },
+  },
+});
+
+// Mesh
+const water = new Mesh(waterGeometry, waterMaterial);
+water.rotation.x = -Math.PI * 0.5;
+scene.add(water);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new PerspectiveCamera(100, sizes.width / sizes.height, 0.1, 100);
+camera.position.set(1, 1, 1);
+scene.add(camera);
+
+/**
+ * Animate
+ */
+const clock = new Clock();
+
+// Debug
+const gui = new dat.GUI({ width: 400 });
+gui.addColor(debugObject, "depthColor").onChange(() => {
+  waterMaterial.uniforms.uDepthColor.value.set(debugObject.depthColor);
+});
+gui.addColor(debugObject, "surfaceColor").onChange(() => {
+  waterMaterial.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor);
+});
+gui
+  .add(waterMaterial.uniforms.uBigWavesElevation, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uBigWavesElevation");
+gui
+  .add(waterMaterial.uniforms.uBigWavesFrequency.value, "x")
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name("uBigWavesFrequencyX");
+gui
+  .add(waterMaterial.uniforms.uBigWavesFrequency.value, "y")
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name("uBigWavesFrequencyY");
+gui
+  .add(waterMaterial.uniforms.uBigWavesSpeed, "value")
+  .min(0)
+  .max(4)
+  .step(0.001)
+  .name("uBigWavesSpeed");
+gui
+  .add(waterMaterial.uniforms.uSmallWavesElevation, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uSmallWavesElevation");
+gui
+  .add(waterMaterial.uniforms.uSmallWavesFrequency, "value")
+  .min(0)
+  .max(30)
+  .step(0.001)
+  .name("uSmallWavesFrequency");
+gui
+  .add(waterMaterial.uniforms.uSmallWavesSpeed, "value")
+  .min(0)
+  .max(4)
+  .step(0.001)
+  .name("uSmallWavesSpeed");
+gui
+  .add(waterMaterial.uniforms.uSmallIterations, "value")
+  .min(0)
+  .max(5)
+  .step(1)
+  .name("uSmallIterations");
+gui
+  .add(waterMaterial.uniforms.uColorOffset, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uColorOffset");
+gui
+  .add(waterMaterial.uniforms.uColorMultiplier, "value")
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name("uColorMultiplier");
+
 export default function renderWaterScene(canvasRef: Ref) {
+  /**
+   * DOM
+   */
   onMounted(() => {
     const canvas = canvasRef.value;
-
-    /**
-     * Base
-     */
-    // Debug
-    const gui = new dat.GUI({ width: 400 });
-
-    // Canvas
-
-    // Scene
-    const scene = new Scene();
-
-    /**
-     * Water
-     */
-    // Geometry
-    const waterGeometry = new PlaneBufferGeometry(2, 2, 512, 512);
-
-    // Colors
-    const debugObject = {
-      depthColor: "#186691",
-      surfaceColor: "#9bd8ff",
-    };
-
-    gui.addColor(debugObject, "depthColor").onChange(() => {
-      waterMaterial.uniforms.uDepthColor.value.set(debugObject.depthColor);
-    });
-    gui.addColor(debugObject, "surfaceColor").onChange(() => {
-      waterMaterial.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor);
-    });
-
-    // Material
-    const waterMaterial = new ShaderMaterial({
-      vertexShader: waterVertexShader,
-      fragmentShader: waterFragmentShader,
-      uniforms: {
-        uTime: { value: 0 },
-
-        uBigWavesElevation: { value: 0.2 },
-        uBigWavesFrequency: { value: new Vector2(4, 1.5) },
-        uBigWavesSpeed: { value: 0.75 },
-
-        uSmallWavesElevation: { value: 0.15 },
-        uSmallWavesFrequency: { value: 3 },
-        uSmallWavesSpeed: { value: 0.2 },
-        uSmallIterations: { value: 4 },
-
-        uDepthColor: { value: new Color(debugObject.depthColor) },
-        uSurfaceColor: { value: new Color(debugObject.surfaceColor) },
-        uColorOffset: { value: 0.08 },
-        uColorMultiplier: { value: 5 },
-      },
-    });
-
-    gui
-      .add(waterMaterial.uniforms.uBigWavesElevation, "value")
-      .min(0)
-      .max(1)
-      .step(0.001)
-      .name("uBigWavesElevation");
-    gui
-      .add(waterMaterial.uniforms.uBigWavesFrequency.value, "x")
-      .min(0)
-      .max(10)
-      .step(0.001)
-      .name("uBigWavesFrequencyX");
-    gui
-      .add(waterMaterial.uniforms.uBigWavesFrequency.value, "y")
-      .min(0)
-      .max(10)
-      .step(0.001)
-      .name("uBigWavesFrequencyY");
-    gui
-      .add(waterMaterial.uniforms.uBigWavesSpeed, "value")
-      .min(0)
-      .max(4)
-      .step(0.001)
-      .name("uBigWavesSpeed");
-
-    gui
-      .add(waterMaterial.uniforms.uSmallWavesElevation, "value")
-      .min(0)
-      .max(1)
-      .step(0.001)
-      .name("uSmallWavesElevation");
-    gui
-      .add(waterMaterial.uniforms.uSmallWavesFrequency, "value")
-      .min(0)
-      .max(30)
-      .step(0.001)
-      .name("uSmallWavesFrequency");
-    gui
-      .add(waterMaterial.uniforms.uSmallWavesSpeed, "value")
-      .min(0)
-      .max(4)
-      .step(0.001)
-      .name("uSmallWavesSpeed");
-    gui
-      .add(waterMaterial.uniforms.uSmallIterations, "value")
-      .min(0)
-      .max(5)
-      .step(1)
-      .name("uSmallIterations");
-
-    gui
-      .add(waterMaterial.uniforms.uColorOffset, "value")
-      .min(0)
-      .max(1)
-      .step(0.001)
-      .name("uColorOffset");
-    gui
-      .add(waterMaterial.uniforms.uColorMultiplier, "value")
-      .min(0)
-      .max(10)
-      .step(0.001)
-      .name("uColorMultiplier");
-
-    // Mesh
-    const water = new Mesh(waterGeometry, waterMaterial);
-    water.rotation.x = -Math.PI * 0.5;
-    scene.add(water);
-
-    /**
-     * Sizes
-     */
-    const sizes = {
-      width: window.innerWidth,
-      height: window.innerHeight,
-    };
 
     window.addEventListener("resize", () => {
       // Update sizes
@@ -162,19 +175,6 @@ export default function renderWaterScene(canvasRef: Ref) {
       renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
     });
 
-    /**
-     * Camera
-     */
-    // Base camera
-    const camera = new PerspectiveCamera(
-      100,
-      sizes.width / sizes.height,
-      0.1,
-      100
-    );
-    camera.position.set(1, 1, 1);
-    scene.add(camera);
-
     // Controls
     const controls = new OrbitControls(camera, canvas);
     controls.enableDamping = true;
@@ -182,16 +182,9 @@ export default function renderWaterScene(canvasRef: Ref) {
     /**
      * Renderer
      */
-    const renderer = new WebGLRenderer({
-      canvas: canvas,
-    });
+    const renderer = new WebGLRenderer({ canvas });
     renderer.setSize(sizes.width, sizes.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-    /**
-     * Animate
-     */
-    const clock = new Clock();
 
     const tick = () => {
       const elapsedTime = clock.getElapsedTime();
@@ -200,6 +193,7 @@ export default function renderWaterScene(canvasRef: Ref) {
       waterMaterial.uniforms.uTime.value = elapsedTime;
 
       // Update controls
+
       controls.update();
 
       // Render
